f614f3b3ffa4748e8edfbf4124485e67036a16df
Razan Raid <razanraid990@gmail.com>
2023-08-23 21:15:16 +0400

    added sample data extension.

---
+++ ToDo App/models and modelviews/Todo+Sample.swift
@@ -0,0 +1,14 @@
+//
+//  Todo+Sample.swift
+//  ToDo App
+//
+//  Created by Razan Raid on 23/08/2023.
+//
+
+import Foundation
+
+extension Todo{
+    static let sampleTodos = [
+        Todo(title: "hello! this is a todo.",subtitle: "click on todo to further edit it.")
+    ]
+}
