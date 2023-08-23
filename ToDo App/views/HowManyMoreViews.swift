6605e6daf004c36cb7c7984aae15f80f152d9df4
Razan Raid <razanraid990@gmail.com>
2023-08-23 21:46:23 +0400

    added how many todos left view and added it to tab view.

---
+++ ToDo App/views/HowManyMoreViews.swift
@@ -0,0 +1,20 @@
+//
+//  HowManyMoreViews.swift
+//  ToDo App
+//
+//  Created by Razan Raid on 23/08/2023.
+//
+
+import SwiftUI
+
+struct HowManyMoreViews: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HowManyMoreViews_Previews: PreviewProvider {
+    static var previews: some View {
+        HowManyMoreViews()
+    }
+}
